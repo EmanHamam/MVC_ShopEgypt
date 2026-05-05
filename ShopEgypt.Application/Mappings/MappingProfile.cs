397b945e76a0384d6f89e386ec96b2e41602d296
EmanHamam <emanhamam109@gmail.com>
2026-05-05 04:34:02 +0300

    (feat):Update in initialArch

---
+++ ShopEgypt.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ShopEgypt.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            #region Cart Profile
+            //CreateMap<Cart, CartDto>()
+
+            //.ForMember(dest => dest.Status,
+            //    opt => opt.MapFrom(src => src.Status.ToString()));
+
+            
+
+
+            #endregion
+
+
+            
+        }
+    }
+
+}
--- ShopEgypt/Program.cs
+++ ShopEgypt/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ShopEgypt.Data.Context;
+using ShopEgypt.Infrastructure.ServiceRegistration;
 
 namespace ShopEgypt
 {
@@ -11,13 +12,19 @@ namespace ShopEgypt
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
+
+            //Services Registration
+            builder.Services.AddInfrastructureServices(builder.Configuration);
+
+
+
+
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
