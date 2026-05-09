747ac792aba9b1c8b741d4b4bc730df1c27f0987
EmanHamam <emanhamam109@gmail.com>
2026-05-10 02:46:17 +0300

    (feat):Add Main Layout

--- ShopEgypt/wwwroot/js/site.js
+++ ShopEgypt/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function () {
+    const menuBtn = document.getElementById("mobileMenuBtn");
+    const mobileNav = document.getElementById("mobileNav");
+
+    if (menuBtn && mobileNav) {
+        menuBtn.addEventListener("click", function () {
+            if (mobileNav.style.display === "block") {
+                mobileNav.style.display = "none";
+            } else {
+                mobileNav.style.display = "block";
+            }
+        });
+    }
+});
