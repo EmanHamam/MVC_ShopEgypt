7e9fbb0c23be721571dad95977a1cbcc298e1d0c
MostafaHendy3 <mostafahendy9991@gmail.com>
2026-05-11 02:14:28 +0300

    updated mig

--- ShopEgypt.Infrastructure/Migrations/20260510224934_hendyMig.Designer.cs
+++ ShopEgypt.Infrastructure/Migrations/20260510224934_hendyMig.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopEgypt.Data.Context;
 
@@ -11,9 +12,11 @@ using ShopEgypt.Data.Context;
 namespace ShopEgypt.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260510224934_hendyMig")]
+    partial class hendyMig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -214,6 +217,7 @@ namespace ShopEgypt.Infrastructure.Migrations
                         .HasColumnType("bit");
 
                     b.Property<string>("FirstName")
+                        .IsRequired()
                         .HasMaxLength(255)
                         .HasColumnType("nvarchar(255)");
 
