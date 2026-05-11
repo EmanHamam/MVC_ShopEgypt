b3370050b2ffac3520785b63e0924b5870d34c91
MostafaHendy3 <mostafahendy9991@gmail.com>
2026-05-11 21:24:46 +0300

    orders

---
+++ ShopEgypt.Application/DTOs/OrdersDTO/AddressDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ShopEgypt.Application.DTOs.OrdersDTO
+{
+    public class AddressDTO
+    {
+        [Key]
+        public string? Street { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? ZipCode { get; set; }
+        public string? Country { get; set; }
+    }
+}
