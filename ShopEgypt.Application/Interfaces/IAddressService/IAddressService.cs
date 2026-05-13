e551aa8af185c5159473ee5b06d310490399ab1f
MostafaHendy3 <mostafahendy9991@gmail.com>
2026-05-14 00:20:05 +0300

    Address initated

---
+++ ShopEgypt.Application/Interfaces/IAddressService/IAddressService.cs
@@ -0,0 +1,14 @@
+﻿using ShopEgypt.Application.DTOs.OrdersDTO;
+using ShopEgypt.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopEgypt.Application.Interfaces.IAddressService
+{
+    public interface IAddressService
+    {
+        public Task<AddressDTO> TryGetSavedAddressAsync(string userId);
+        public Task TrySaveAddAddressAsync(string userId, AddressDTO AddressDTO);
+    }
+}
---
+++ ShopEgypt.Infrastructure/Services/AddressService/AddressService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopEgypt.Infrastructure.Services.AddressService
+{
+    internal class AddressService
+    {
+    }
+}
